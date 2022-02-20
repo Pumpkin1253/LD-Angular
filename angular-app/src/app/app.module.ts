bb7d976656176f9f42d53a6b90b67dd67131e4ef
Mykyta Matvieiev <mykyta_matvieiev@epam.com>
2022-02-20 18:34:12 +0200

    added ngrx store devtools

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS }   from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment'; // Angular CLI environment
 
 import { AppComponent } from './app.component';
 import { CourseModule } from './features/course/course.module';
@@ -11,7 +14,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { UserModule } from './user/user.module';
 import { TokenInterceptor } from './auth/interceptors/token.interceptor';
 import { effects, reducers } from './store';
-import { EffectsModule } from '@ngrx/effects';
 
 
 
@@ -29,7 +31,12 @@ import { EffectsModule } from '@ngrx/effects';
     HttpClientModule,
     UserModule,
     StoreModule.forRoot(reducers),
-    EffectsModule.forRoot(effects)
+    EffectsModule.forRoot(effects),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
   ],
   providers: [
     {
